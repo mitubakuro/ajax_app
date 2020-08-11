7f00397d5514b14fa8ff9e01674deaf11ec23659
mitubakuro <nakano1113mituba@gmail.com>
2020-08-12 02:56:16 +0900

    ＋＠

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -16,6 +16,7 @@ function check() {
       const postId = post.getAttribute("data-id");
  
       // Ajaxに必要なオブジェクトを生成している
+      const XHR = new XMLHttpRequest();
  
       // openでリクエストを初期化する
       XHR.open("GET", `/posts/${postId}`, true);
