658ba7ddf6429e3aa2a13865d8e3c5d1651a2c23
mitubakuro <nakano1113mituba@gmail.com>
2020-08-12 12:20:41 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,6 @@ class PostsController < ApplicationController
     # redirect_to action: :index
     post = Post.create(content: params[:content], checked: false)
     render json:{ post: post }
-
   end
 
   def checked
